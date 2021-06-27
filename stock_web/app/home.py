e8a48a385c1cd9cbc5a084fe28d5f337f14b95bf
Bo Tang <tb_ldxx@126.com>
2021-06-27 20:26:30 +0800

    home page basic information first draft

--- stock_web/app/home.py
+++ stock_web/app/home.py
@@ -1,9 +1,15 @@
-from flask import render_template
+from flask import render_template, abort
 import requests
+import json
 
 def home_page_process():
 
-    response = requests.get("http://127.0.0.1:5000/test")
-    aaa = response.content.decode("utf-8")
+    response = requests.get("http://127.0.0.1:5051/home")
+    return_data = json.loads(response.content.decode("utf-8"))
 
-    return render_template("home.html", tb_test=aaa)
+    if return_data["result"]:
+        data = return_data['data']
+        return render_template("home.html", data=data)
+    else:
+        abort(400)
+
